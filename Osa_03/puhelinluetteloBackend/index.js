3e76857d8f795146404a6ea9c25f7ef03197b820
Kai Jukarainen <kai.jukarainen@gmail.com>
2022-12-22 13:49:08 +0200

    first commit

---
+++ Osa_03/puhelinluetteloBackend/index.js
@@ -0,0 +1,153 @@
+const morgan = require('morgan')
+const express = require('express')
+const { response } = require('express')
+const cors = require('cors')
+require("dotenv").config();
+const Person = require('./models/person')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+
+morgan.token('postData', function (req, res, param) {
+    return JSON.stringify(req.body)
+  })
+  
+app.use(morgan(
+    'METHOD: :method - URL: :url - STATUS: :status - RESPONSE TIME: :response-time[3] ms - POSTED DATA: :postData'
+  ))
+
+/*
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada LoveLace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122",
+    }
+]
+*/
+
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>FullStackOpen22 PhoneBook </h1>')
+})
+
+app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
+      response.send(`<p> Phonebook has info for ${persons.length} people </p> <p>${Date()}</p>`)
+  })
+})
+
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+      response.json(persons)
+  })
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+      .then(person => {
+          if (person) {
+              response.json(person)
+          } else {
+              response.status(404).end()
+          }
+      })
+      .catch(error => next(error))
+})
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+          response.status(204).end()
+      })
+      .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+      name: body.name,
+      number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+          response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+      return response.status(400).json({
+          error: 'name is missing'
+      })
+  }
+
+  if (!body.number) {
+      return response.status(400).json({
+          error: 'number is missing'
+      })
+  }
+
+  const person = new Person({
+      name: body.name,
+      number: body.number
+  })
+
+  person.save().then(savedPerson => {
+      response.json(savedPerson)
+  })
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
