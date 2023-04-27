df6dc1679aeaca413ac59fcfc54de2ce056a905e
Kai Jukarainen <kai.jukarainen@gmail.com>
2023-04-27 09:42:50 +0300

    8.23

--- Osa_08/library-backend/index.js
+++ Osa_08/library-backend/index.js
@@ -1,156 +1,56 @@
-const {
-  ApolloServer,
-  gql,
-  UserInputError,
-  AuthenticationError
-} = require('apollo-server');
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const { ApolloServer } = require('apollo-server-express');
+const { createServer } = require('http');
+const { useServer: useGraphQlWsOnWsServer } = require('graphql-ws/lib/use/ws');
+const { WebSocketServer } = require('ws');
+const express = require('express');
 const mongoose = require('mongoose');
-const Author = require('./models/author');
-const Book = require('./models/book');
-const User = require('./models/user');
+const { context, schema } = require('./graphql');
+const { MONGODB_URI, PORT } = require('./utils/config');
 
-dotenv.config();
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
-const handleDatabaseError = (error) => {
-  if (error.name === 'ValidationError')
-    throw new UserInputError(error.message, {
-      invalidArgs: Object.keys(error.errors).map(
-        (key) => error.errors[key].path
-      )
-    });
-  else if (error.name === 'MongoServerError' && error.code === 11000)
-    throw new UserInputError(error.message, {
-      invalidArgs: Object.keys(error.keyPattern)
-    });
+const app = express();
 
-  throw error;
-};
+const path = '/';
 
-const typeDefs = gql`
-  type Author {
-    bookCount: Int!
-    born: Int
-    id: ID!
-    name: String!
-  }
-  type Book {
-    author: Author!
-    genres: [String!]!
-    id: ID!
-    published: Int!
-    title: String!
-  }
-  type User {
-    username: String!
-    favouriteGenre: String!
-    id: ID!
-  }
-  type Token {
-    value: String!
-  }
-  type Mutation {
-    editAuthor(name: String!, setBornTo: Int!): Author
-    addBook(
-      author: String!
-      genres: [String!]!
-      published: Int!
-      title: String!
-    ): Book
-    createUser(username: String!, favouriteGenre: String!): User
-    login(username: String!, password: String!): Token
-  }
-  type Query {
-    authorCount: Int!
-    allAuthors: [Author!]!
-    bookCount: Int!
-    allBooks(author: String, genre: String): [Book!]!
-    me: User
-  }
-`;
+const httpServer = createServer(app);
 
-const resolvers = {
-  Mutation: {
-    editAuthor: async (_root, { name, setBornTo }, { authUser }) => {
-      if (!authUser) throw new AuthenticationError('unauthenticated');
-
-      return Author.findOneAndUpdate(
-        { name },
-        { born: setBornTo },
-        { new: true }
-      );
-    },
-    addBook: async (
-      _root,
-      { author: name, genres, published, title },
-      { authUser }
-    ) => {
-      if (!authUser) throw new AuthenticationError('unauthenticated');
-
-      let author = await Author.findOne({ name });
-
-      if (!author) {
-        author = new Author({ name });
-        await author.save().catch(handleDatabaseError);
-      }
-
-      const book = new Book({ author, genres, published, title });
-      await book.save().catch(handleDatabaseError);
-
-      return book;
-    },
-    createUser: async (_root, { username, favouriteGenre }) => {
-      const user = new User({ username, favouriteGenre });
-      console.log(newUser)
-      return user.save().catch(handleDatabaseError);
-    },
-    login: async (_root, { username, password }) => {
-      const user = await User.findOne({ username });
-
-      if (!user || password !== 'testuserpassword')
-        throw new UserInputError('invalid credentials');
-
-      return { value: jwt.sign({ id: user._id }, process.env.JWT_SECRET) };
-    }
-  },
-  Query: {
-    authorCount: () => Author.collection.countDocuments(),
-    allAuthors: async () => Author.find({}),
-    bookCount: () => Book.collection.countDocuments(),
-    allBooks: async (_root, { author, genre }) => {
-      let filter = {};
-      if (author) filter.author = await Author.findOne({ name: author });
-      if (genre) filter.genres = genre;
-      return Book.find(filter);
-    },
-    me: (_root, _args, { authUser }) => authUser
-  },
-  Author: {
-    bookCount: async (parent) => Book.countDocuments({ author: parent })
-  },
-  Book: {
-    author: async (parent) => {
-      await parent.populate('author');
-      return parent.author;
-    }
-  }
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const auth = req?.headers?.authorization;
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const { id } = jwt.verify(auth.substring(7), process.env.JWT_SECRET);
-      const authUser = await User.findById(id);
-      return { authUser };
-    }
-  }
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path,
 });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+const { dispose: disposeWsServer } = useGraphQlWsOnWsServer(
+  { schema },
+  wsServer
+);
+
+const apolloServer = new ApolloServer({
+  context,
+  schema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await disposeWsServer();
+          },
+        };
+      },
+    },
+  ],
 });
+
+(async () => {
+  await apolloServer.start();
+
+  app.use(apolloServer.getMiddleware({ path }));
+
+  await httpServer.listen({ port: PORT });
+
+  console.log(
+    `Server ready at http://localhost:${PORT + apolloServer.graphqlPath}`
+  );
+})();
